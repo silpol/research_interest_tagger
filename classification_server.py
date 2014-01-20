031f9b4fc0f1161742f65be1eebd23f3c3094e72
David Judd <david@academia.edu>
2014-01-20 15:37:53 -0800

    Add health check and debug printing

--- classification_server.py
+++ classification_server.py
@@ -22,6 +22,9 @@ from twisted.web import server, resource
 
 NUM_PROCESSES = 4
 PORT = 6606
+DEBUG = True
+
+SAMPLE_TEXT = """In this paper, we present Google, a prototype of a large-scale search engine which makes heavy use of the structure present in hypertext. Google is designed to crawl and index the Web efficiently and produce much more satisfying search results than existing systems. The prototype with a full text and hyperlink database of at least 24 million pages is available at http://google.stanford.edu/ To engineer a search engine is a challenging task. Search engines index tens to hundreds of millions of web pages involving a comparable number of distinct terms. They answer tens of millions of queries every day. Despite the importance of large-scale search engines on the web, very little academic research has been done on them. Furthermore, due to rapid advance in technology and web proliferation, creating a web search engine today is very different from three years ago. This paper provides an in-depth description of our large-scale web search engine -- the first such detailed public description we know of to date. Apart from the problems of scaling traditional search techniques to data of this magnitude, there are new technical challenges involved with using the additional information present in hypertext to produce better search results. This paper addresses this question of how to build a practical large-scale system which can exploit the additional information present in hypertext. Also we look at the problem of how to effectively deal with uncontrolled hypertext collections where anyone can publish anything they want."""
 
 model_path = sys.argv[1]
 perceptron, category_list = joblib.load("%s/Perceptron.model" % model_path, mmap_mode='r')
@@ -48,7 +51,16 @@ class ClassifyResource(resource.Resource):
     def render_POST(self, request):
         text = request.args['text'][0]
         pred = get_prediction(text)
+        if DEBUG:
+          print "Classified text '%s...' as %s" % (text[:25], ','.join(pred))
         return json.dumps({ 'categories': pred }) + "\n"
+
+class HealthResource(resource.Resource):
+    def render_GET(self, request):
+        health = 'ok' if u'the internet' in get_prediction(SAMPLE_TEXT) else 'unexpected prediction'
+        if DEBUG:
+          print "Health check: %s" % health
+        return json.dumps({ 'health': health }) + "\n"
 
 
 # Creates NUM_PROCESSES separate Twisted processes listening on the same port
@@ -56,10 +68,14 @@ class ClassifyResource(resource.Resource):
 def main(fd = None):
     root = resource.Resource()
     root.putChild('classify', ClassifyResource())
+    root.putChild('health', HealthResource())
     factory = server.Site(root)
 
     if fd is None:
         port = reactor.listenTCP(PORT, factory)
+
+        if DEBUG:
+          print "Started listening on %d" % PORT
 
         fds = dict([(i,i) for i in range(NUM_PROCESSES-2)])
         fds[port.fileno()] = port.fileno()
